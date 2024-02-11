e1af087c3d5962d391e828e5ef3bc74f7e90d7f6
BuildTools <unconfigured@null.spigotmc.org>
2024-02-11 19:14:35 +0200

    Cookie_clicker

---
+++ Cookie Clicker/variables.py
@@ -0,0 +1,19 @@
+screen_width = 1280
+screen_height = 720
+
+running = True
+# cookie variables
+cookie_color = (157, 126, 103)
+cookie_light = (181, 145, 118)
+cookie_position = [1000, 100]
+cookie_size = [100,100]
+# main
+cookies = 10000
+cookies_per_click = 10
+clicks_per_second = 0
+clicker_list = []
+
+item_position = [200, 100]
+item_size = [200, 50]
+item_color = (136, 231, 235)
+item_light = (166, 251, 255)
